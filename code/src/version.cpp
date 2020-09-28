1633b74ab2a797bd561f77fe11589dd3389beb6e
jomjol <30766535+jomjol@users.noreply.github.com>
2020-09-28 20:03:38 +0200

    Update to v21.1

--- code/src/version.cpp
+++ code/src/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="cefe125";
+const char* GIT_REV="bafd67b";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2020-09-28 19:32";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2020-09-28 19:57";
