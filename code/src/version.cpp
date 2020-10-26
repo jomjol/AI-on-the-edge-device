a23d7ee6e270b7a097b3c11f529fa0d5c0386d48
jomjol <30766535+jomjol@users.noreply.github.com>
2020-10-26 18:29:52 +0100

    v3.1.0

--- code/src/version.cpp
+++ code/src/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="2614481";
+const char* GIT_REV="8f51895";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="master";
-const char* BUILD_TIME="2020-10-14 18:40";
+const char* BUILD_TIME="2020-10-26 18:26";
