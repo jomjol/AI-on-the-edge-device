05a0f6fa623f4d4583be1513ed9a3524273ee440
jomjol <30766535+jomjol@users.noreply.github.com>
2020-11-03 22:17:57 +0100

    Bug-Fix

--- code/src/version.cpp
+++ code/src/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="3cec93e";
+const char* GIT_REV="2ab2f07";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="master";
-const char* BUILD_TIME="2020-10-26 18:31";
+const char* BUILD_TIME="2020-11-03 22:12";
