2ae304dcf51f91597e469f0a6a0d6b4a98e39f4b
jomjol <30766535+jomjol@users.noreply.github.com>
2020-12-02 21:57:55 +0100

    Update

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="f6f3e23";
+const char* GIT_REV="ffc15aa";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2020-12-02 21:50";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2020-12-02 21:55";
