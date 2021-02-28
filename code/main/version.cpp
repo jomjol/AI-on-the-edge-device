94dde53c2102a25eb757a96b484120d2bbbff587
jomjol <30766535+jomjol@users.noreply.github.com>
2021-02-28 18:40:55 +0100

    Update rolling

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="9dbad05";
+const char* GIT_REV="e47eaa3";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling-camera-speedup";
-const char* BUILD_TIME="2021-02-21 21:40";
+const char* GIT_BRANCH="rolling";
+const char* BUILD_TIME="2021-02-28 18:27";
