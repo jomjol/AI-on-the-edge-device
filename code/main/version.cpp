3b3d924f4037fdc26310e214422a5c7edff3b6a0
jomjol <30766535+jomjol@users.noreply.github.com>
2021-03-16 21:14:09 +0100

    final update

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-const char* GIT_REV="5f0f180";
+const char* GIT_REV="60701bc";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="master";
-const char* BUILD_TIME="2021-03-14 12:56";
-=======
-const char* GIT_REV="5be56d9";
-const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-03-16 20:48";
->>>>>>> rolling
+const char* BUILD_TIME="2021-03-16 21:11";
