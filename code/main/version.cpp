9862ae8e7acffb2aab1445efd160233d4ab3ae83
jomjol <30766535+jomjol@users.noreply.github.com>
2021-03-25 20:58:44 +0100

    Update to v6.5.0

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="c6ae989";
+const char* GIT_REV="7bc4e63";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-03-25 20:40";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2021-03-25 20:52";
