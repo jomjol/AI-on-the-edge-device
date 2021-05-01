1459bb15c14eda0ddf2cfd57f4b6c1e953dbb58a
jomjol <30766535+jomjol@users.noreply.github.com>
2021-05-01 17:32:22 +0200

    Prepare v6.7.1

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="ba7d429";
+const char* GIT_REV="ce5f3c4";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-04-30 22:50";
+const char* BUILD_TIME="2021-05-01 17:30";
