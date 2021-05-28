e5ff8f216448083cc337801ea14252c6cf51edf1
jomjol <30766535+jomjol@users.noreply.github.com>
2021-05-28 19:56:10 +0200

    Update Firmware

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="6e0a7a7";
+const char* GIT_REV="a000252";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-05-28 19:48";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2021-05-28 19:53";
