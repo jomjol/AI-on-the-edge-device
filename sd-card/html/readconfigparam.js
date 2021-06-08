c25adfe28a88570a1bac62b5ff6dd39399ff88db
Zwer2k <jr.github.com@topofweb.de>
2021-06-09 00:16:31 +0200

    add interrupt to gpio config bugfix in gpio config

--- sd-card/html/readconfigparam.js
+++ sd-card/html/readconfigparam.js
@@ -97,7 +97,7 @@ function ParseConfig() {
      category[catname]["enabled"] = false;
      category[catname]["found"] = false;
      param[catname] = new Object();
-     ParamAddValue(param, catname, "IO16", 2, [null, /^[a-zA-Z0-9_-]*$/]);
+     ParamAddValue(param, catname, "IO16", 3, [null, null, /^[a-zA-Z0-9_-]*$/]);
 
      var catname = "AutoTimer";
      category[catname] = new Object(); 
