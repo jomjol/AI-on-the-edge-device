737dcc76b8848764ca6a233521a0af3aafffb258
jomjol <30766535+jomjol@users.noreply.github.com>
2021-07-04 08:12:36 +0200

    Revert "rolling 20210704 bugfix"
    This reverts commit b42f17916b3c615f30594ff51a6d30ee52c26d89.

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="2c6ce6f";
+const char* GIT_REV="f243f4b";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-07-04 08:03";
+const char* BUILD_TIME="2021-07-04 06:46";
