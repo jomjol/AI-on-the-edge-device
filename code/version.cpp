a91f99faab3d8206ee7aaedfb5141ade75ee26de
jomjol <30766535+jomjol@users.noreply.github.com>
2021-08-01 21:49:29 +0200

    update

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="d4b5ec2";
+const char* GIT_REV="17a87b2";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-08-01 21:38";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2021-08-01 21:47";
