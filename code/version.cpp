7816e53db73d22f002e73278e06601fcab7b8deb
jomjol <30766535+jomjol@users.noreply.github.com>
2021-08-24 08:37:18 +0200

    v8.2.0

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="0dac0e8";
+const char* GIT_REV="7ae08e5";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-08-24 08:31";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2021-08-24 08:35";
