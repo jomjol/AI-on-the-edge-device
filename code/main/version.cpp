776931c0ad3722e359f2eab0fb6be56323f031b9
jomjol <30766535+jomjol@users.noreply.github.com>
2021-12-02 21:56:05 +0100

    v9.2.0

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="cad534b";
+const char* GIT_REV="e22b4b6";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-12-02 21:50";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2021-12-02 21:53";
