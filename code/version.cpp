cad534bffe303ccfaec7aba9a2fd5a25f3093afc
jomjol <30766535+jomjol@users.noreply.github.com>
2021-12-02 21:47:37 +0100

    update wiki

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="58124d2";
+const char* GIT_REV="3b44adb";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2021-11-24 07:21";
+const char* BUILD_TIME="2021-12-02 21:26";
