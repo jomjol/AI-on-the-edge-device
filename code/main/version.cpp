04c564936aefa30ffa83980b68dfc9edb2c67638
jomjol <30766535+jomjol@users.noreply.github.com>
2021-12-30 18:29:01 +0100

    Master 10.0.0

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="58a0297";
+const char* GIT_REV="9571389";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="update_espressif";
-const char* BUILD_TIME="2021-12-30 09:03";
+const char* GIT_BRANCH="rolling";
+const char* BUILD_TIME="2021-12-30 18:15";
