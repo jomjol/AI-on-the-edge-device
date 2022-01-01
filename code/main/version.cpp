8e8897c70df321fed1c7f84cba09584fd1d68dcd
jomjol <30766535+jomjol@users.noreply.github.com>
2022-01-01 08:57:07 +0100

    v10.0.2

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="98f9274";
-const char* GIT_TAG="v10.0.1";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2022-01-01 08:46";
+const char* GIT_REV="eac5134";
+const char* GIT_TAG="";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2022-01-01 08:54";
