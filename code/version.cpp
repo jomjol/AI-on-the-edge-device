4d997d9473eef0901025a510c07bb25f53005f49
jomjol <30766535+jomjol@users.noreply.github.com>
2022-01-14 20:28:01 +0100

    v10.2.0

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="2daf6c8";
-const char* GIT_TAG="v10.1.0";
+const char* GIT_REV="e79c86c";
+const char* GIT_TAG="";
 const char* GIT_BRANCH="wifi-mod";
-const char* BUILD_TIME="2022-01-14 20:11";
+const char* BUILD_TIME="2022-01-14 20:20";
