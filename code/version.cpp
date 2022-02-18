cbe884ad635b962c5e5efb8ccf994581290e754d
jomjol <30766535+jomjol@users.noreply.github.com>
2022-02-18 21:22:00 +0100

    v10.5.0

--- code/version.cpp
+++ code/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="bcb1d98";
+const char* GIT_REV="1dd703b";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="rolling";
-const char* BUILD_TIME="2022-02-18 21:01";
+const char* GIT_BRANCH="master";
+const char* BUILD_TIME="2022-02-18 21:15";
