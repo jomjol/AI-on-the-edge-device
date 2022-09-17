36f56183766f929c6807cc58b9b0399ff079c7c6
jomjol <30766535+jomjol@users.noreply.github.com>
2022-09-17 10:16:31 +0200

    Rolling 20220917

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="36a02d1";
+const char* GIT_REV="0354236";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="master";
-const char* BUILD_TIME="2022-09-17 10:08";
+const char* GIT_BRANCH="rolling";
+const char* BUILD_TIME="2022-09-17 10:12";
