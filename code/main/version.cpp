6938299b722b2b872044fc7580532998972d13c3
Cristian <cmitran@gmail.com>
2022-09-21 23:36:14 +0200

    Few typing corrections and translations

--- code/main/version.cpp
+++ code/main/version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="6cd52a3";
+const char* GIT_REV="01337dd";
 const char* GIT_TAG="";
-const char* GIT_BRANCH="master";
-const char* BUILD_TIME="2022-09-21 07:51";
+const char* GIT_BRANCH="texts-typing-corrections";
+const char* BUILD_TIME="2022-09-21 19:55";
