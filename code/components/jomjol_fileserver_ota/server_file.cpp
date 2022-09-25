efb35b1aa7a3746a827d9bfc74f465b5af38d0cb
CaCO3 <caco3@ruinelli.ch>
2022-09-25 16:21:12 +0200

    corrected max upload size

--- code/components/jomjol_fileserver_ota/server_file.cpp
+++ code/components/jomjol_fileserver_ota/server_file.cpp
@@ -49,8 +49,8 @@ extern "C" {
 
 /* Max size of an individual file. Make sure this
  * value is same as that set in upload_script.html */
-#define MAX_FILE_SIZE   (2000*1024) // 200 KB
-#define MAX_FILE_SIZE_STR "2000KB"
+#define MAX_FILE_SIZE   (8000*1024) // 8 MB
+#define MAX_FILE_SIZE_STR "8MB"
 
 
 /* Scratch buffer size */
