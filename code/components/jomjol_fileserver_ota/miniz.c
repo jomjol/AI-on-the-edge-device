cdbe23ea4286b62f19bbc6c8e1ee850896ea37ff
CaCO3 <caco3@ruinelli.ch>
2022-11-07 20:36:44 +0100

    minor update to 11.0.1 (resp. 3.0.1)

--- code/components/jomjol_fileserver_ota/miniz.c
+++ code/components/jomjol_fileserver_ota/miniz.c
@@ -1124,7 +1124,7 @@ static mz_bool tdefl_compress_lz_codes(tdefl_compressor *d)
         {
             mz_uint s0, s1, n0, n1, sym, num_extra_bits;
             mz_uint match_len = pLZ_codes[0];
-            match_dist = (pLZ_codes[1] | (pLZ_codes[2] << 8));
+            mz_uint match_dist = (pLZ_codes[1] | (pLZ_codes[2] << 8));
             pLZ_codes += 3;
 
             MZ_ASSERT(d->m_huff_code_sizes[0][s_tdefl_len_sym[match_len]]);
