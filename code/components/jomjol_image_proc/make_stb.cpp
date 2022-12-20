4379e2dec22b8a97d5e1409fcb477e0d77abaf15
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2022-12-20 06:47:28 +0100

    STBI_ONLY_JPEG alrready defined in define.h (#1641)
    STBI_ONLY_JPEG alrready defined in define.h

--- code/components/jomjol_image_proc/make_stb.cpp
+++ code/components/jomjol_image_proc/make_stb.cpp
@@ -1,9 +1,7 @@
 #include <stdint.h>
 #include <string>
 
-#define STBI_ONLY_JPEG // (save 2% of Flash)
 #include "../../include/defines.h"
-
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
