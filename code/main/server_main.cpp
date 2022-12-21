443bd67f815850bb98849fe033880a4136ff27a7
CaCO3 <caco3@ruinelli.ch>
2022-12-21 21:26:52 +0100

    show release in log instead of branch (#1660)
    Co-authored-by: CaCO3 <caco@ruinelli.ch>

--- code/main/server_main.cpp
+++ code/main/server_main.cpp
@@ -74,15 +74,7 @@ esp_err_t info_get_handler(httpd_req_t *req)
     }
     else if (_task.compare("FirmwareVersion") == 0)
     {
-        string buf;
-        if (std::string(GIT_TAG) == "") { // Tag not set, show branch
-            buf = "Development-Branch: " + std::string(GIT_BRANCH);
-        }
-        else { // Tag is set, ignore branch
-            buf = "Release: " + std::string(GIT_TAG);
-        }
-        buf = buf + " (Commit: " + std::string(GIT_REV) + ")";
-        httpd_resp_sendstr_chunk(req, buf.c_str());
+        httpd_resp_sendstr_chunk(req, getFwVersion().c_str());
         httpd_resp_sendstr_chunk(req, NULL);  
         return ESP_OK;        
     }
