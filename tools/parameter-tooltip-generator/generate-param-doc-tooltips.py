d7682cbcb5711a5d21db31da8906300b5ab9035b
CaCO3 <caco@ruinelli.ch>
2023-02-22 18:02:50 +0100

    .

--- tools/parameter-tooltip-generator/generate-param-doc-tooltips.py
+++ tools/parameter-tooltip-generator/generate-param-doc-tooltips.py
@@ -7,8 +7,9 @@ import markdown
 
 
 parameterDocsFolder = "AI-on-the-edge-device-docs/param-docs/parameter-pages"
-#htmlTooltipFolder = "html"
-configPage = "../../sd-card/html/edit_config_param.html"
+docsMainFolder = "../../sd-card/html"
+configPage = "edit_config_param.html"
+imagesFolder = "param_tooltips"
 
 htmlTooltipPrefix = """
     <div class="rst-content"><div class="tooltip"><img src="help.png" width="32px"><span class="tooltiptext">
@@ -24,7 +25,7 @@ folders = sorted( filter( os.path.isdir, glob.glob(parameterDocsFolder + '/*') )
 
 
 def generateHtmlTooltip(section, parameter, markdownFile):
-    #print(section, parameter, file)
+    # print(section, parameter, markdownFile)
 
     with open(markdownFile, 'r') as markdownFileHandle:
         markdownFileContent = markdownFileHandle.read()
@@ -36,17 +37,23 @@ def generateHtmlTooltip(section, parameter, markdownFile):
     # Make all links to be opened in a new page
     htmlTooltip = htmlTooltip.replace("a href", "a target=_blank href")
 
+    # Add custom styles
+    htmlTooltip = htmlTooltip.replace("<h3>", "<h3 style=\"margin: 0\">")
+
+    # Update image paths and copy images to right folder
+    if "../img/" in htmlTooltip:
+        htmlTooltip = htmlTooltip.replace("../img/", imagesFolder + "/")
 
     htmlTooltip = htmlTooltipPrefix + htmlTooltip + htmlTooltipSuffix
 
     # Add the tooltip to the config page
-    with open(configPage, 'r') as configPageHandle:
+    with open(docsMainFolder + "/" + configPage, 'r') as configPageHandle:
         configPageContent = configPageHandle.read()
 
-    print("replacing $TOOLTIP_" + section + "_" + parameter + " with the tooltip content...")
+    # print("replacing $TOOLTIP_" + section + "_" + parameter + " with the tooltip content...")
     configPageContent = configPageContent.replace("$TOOLTIP_" + section + "_" + parameter, htmlTooltip)
 
-    with open(configPage, 'w') as configPageHandle:
+    with open(docsMainFolder + "/" + configPage, 'w') as configPageHandle:
         configPageHandle.write(configPageContent)
 
 
@@ -60,6 +67,14 @@ for folder in folders:
 
     files = sorted(filter(os.path.isfile, glob.glob(parameterDocsFolder + "/" + folder + '/*')))
     for file in files:
+        if not ".md" in file: # Skip non-markdown files
+            continue
+
         parameter = file.split("/")[-1].replace(".md", "")
         parameter = parameter.replace("<", "").replace(">", "")
         generateHtmlTooltip(folder, parameter, file)
+
+"""
+Copy images to main folder
+"""
+os.system("cp " + parameterDocsFolder + "/img/* " + docsMainFolder + "/" + imagesFolder)
