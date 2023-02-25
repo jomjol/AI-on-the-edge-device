74ea97385210f916f37563bb58be44d719d10a84
CaCO3 <caco@ruinelli.ch>
2023-02-26 00:39:30 +0100

    .

--- code/components/jomjol_tfliteclass/server_tflite.cpp
+++ code/components/jomjol_tfliteclass/server_tflite.cpp
@@ -388,7 +388,7 @@ esp_err_t handler_wasserzaehler(httpd_req_t *req)
             txt = "<body style=\"font-family: arial\">";
 
             if ((countRounds <= 1) && (*status != std::string("Flow finished"))) { // First round not completed yet
-                txt = "<h3>Please wait for the first round to complete!</h3><h3>Current state: " + *status + "</h3>\n";
+                txt += "<h3>Please wait for the first round to complete!</h3><h3>Current state: " + *status + "</h3>\n";
             }
             else {
                 txt += "<h3>Value</h3>";
