a9402417f0526d4a3efa8a3ee33cd7b52a20c97e
CaCO3 <caco@ruinelli.ch>
2023-03-17 22:57:13 +0100

    switch log level to debug

--- code/components/jomjol_helper/psram.cpp
+++ code/components/jomjol_helper/psram.cpp
@@ -11,9 +11,8 @@ void *malloc_psram_heap(std::string name, size_t size, uint32_t caps) {
 
 	ptr = heap_caps_malloc(size, caps);
     if (ptr != NULL) {
-	  //  LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for " + name + " (end: " + to_string(*ptr + size) + ")");
-	    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
-    }
+	    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for " + name + " (end: " + to_string(*ptr + size) + ")");
+	}
     else {
         LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to allocate " + to_string(size) + " bytes in PSRAM for '" + name + "'!");
     }
@@ -27,9 +26,8 @@ void *calloc_psram_heap(std::string name, size_t n, size_t size, uint32_t caps) 
 
 	ptr = heap_caps_calloc(n, size, caps);
     if (ptr != NULL) {
-	  //  LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for " + name + " (end: " + to_string(*ptr + size) + ")");
-	    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
-    }
+	    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for " + name + " (end: " + to_string(*ptr + size) + ")");
+	}
     else {
         LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to allocate " + to_string(size) + " bytes in PSRAM for '" + name + "'!");
     }
@@ -39,6 +37,6 @@ void *calloc_psram_heap(std::string name, size_t n, size_t size, uint32_t caps) 
 
 
 void free_psram_heap(std::string name, void *ptr) {
+    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Freeing memory in PSRAM used for '" + name + "'...");
     heap_caps_free(ptr);
-    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Freed memory in PSRAM used for '" + name + "'");
 }
