1da2644f1a0d49b3f794ac081ad9c9f23943ba3b
CaCO3 <caco@ruinelli.ch>
2023-03-22 22:31:38 +0100

    removed the stb_image files and re-add them as a submodule. - stb_image.h: Version update 2.25 -> 2.28 - stb_resize.h: Version update 0.96 -> 0.97 - stb_write.h: Version update 1.14 -> 1.16

--- code/components/jomjol_image_proc/make_stb.cpp
+++ code/components/jomjol_image_proc/make_stb.cpp
@@ -4,10 +4,10 @@
 #include "../../include/defines.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../stb/stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "../stb/stb_image_write.h"
 
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
+#include "../stb/stb_image_resize.h"
