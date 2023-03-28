ab54cad0c4dd94e571e55a0d8b674a15ada07ed9
CaCO3 <caco@ruinelli.ch>
2023-03-28 22:18:11 +0200

    .

--- code/main/websocket.cpp
+++ code/main/websocket.cpp
@@ -15,16 +15,13 @@
 static const char *TAG = "WEBSOCKET";
 
 static httpd_handle_t server = NULL;
-static httpd_handle_t my_hd = NULL;
+static httpd_handle_t websocket_handle = NULL;
 
 /*
- * Structure holding server handle
- * and internal socket fd in order
- * to use out of request send
- */
+ * Structure holding server handle and message
+ * in order to use out of request send */
 struct async_resp_arg {
     httpd_handle_t hd;
-    int fd;
     char message[MAX_MESSAGE_LENGTH];
 };
 
@@ -36,7 +33,7 @@ static void websocket_send_pending_message(void *arg) {
     esp_err_t ret;
     struct async_resp_arg *resp_arg = (struct async_resp_arg *)arg;
 
-    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Sending Websocket message: '" + std::string(resp_arg->message) + "'");
+    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Sending Websocket message: '" + std::string(resp_arg->message) + "'");
     
     httpd_ws_frame_t ws_pkt;
     memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
@@ -59,7 +56,7 @@ static void websocket_send_pending_message(void *arg) {
     for (int i = 0; i < fds; i++) {
         int client_info = httpd_ws_get_fd_info(server, client_fds[i]);
         if (client_info == HTTPD_WS_CLIENT_WEBSOCKET) {
-            httpd_ws_send_frame_async(my_hd, client_fds[i], &ws_pkt);
+            httpd_ws_send_frame_async(websocket_handle, client_fds[i], &ws_pkt);
         }
     }
 
@@ -70,11 +67,11 @@ static void websocket_send_pending_message(void *arg) {
 esp_err_t schedule_websocket_message(std::string message) {
     esp_err_t ret;
 
-    if (my_hd == NULL) { // No websocket connecten open
+    if (websocket_handle == NULL) { // No websocket connecten open
         return ESP_OK;
     }
 
-    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Scheduled websocket message: '" + message + "'");
+    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Scheduled websocket message: '" + message + "'");
 
     struct async_resp_arg *resp_arg = (struct async_resp_arg *)malloc_psram_heap("websocket msg", 
             sizeof(struct async_resp_arg), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
@@ -85,7 +82,7 @@ esp_err_t schedule_websocket_message(std::string message) {
    
     strncpy(resp_arg->message, message.c_str(), MAX_MESSAGE_LENGTH);
 
-    ret = httpd_queue_work(my_hd, websocket_send_pending_message, resp_arg);
+    ret = httpd_queue_work(websocket_handle, websocket_send_pending_message, resp_arg);
     if (ret != ESP_OK) {
         LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Websocket Scheduling failed: " + std::to_string(ret) + "!");
         free_psram_heap("websocket msg", resp_arg);
@@ -97,42 +94,9 @@ esp_err_t schedule_websocket_message(std::string message) {
 
 static esp_err_t ws_handler(httpd_req_t *req) {
     if (req->method == HTTP_GET) {
-        LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Handshake done, the new websocket connection was opened");
-        my_hd = req->handle;
-        return ESP_OK;
+        LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Handshake done, the new websocket connection was opened");
+        websocket_handle = req->handle;
     }
-
-    httpd_ws_frame_t ws_pkt;
-    uint8_t *buf = NULL;
-    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
-    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
-    if (ret != ESP_OK)
-    {
-        LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to get frame len with: " + std::to_string(ret) + "!");
-        return ret;
-    }
-
-    if (ws_pkt.len)
-    {
-        buf = (uint8_t *)calloc_psram_heap("websocket buf", 1, ws_pkt.len + 1, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
-        if (buf == NULL) {
-            LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to calloc memory for websocket buffer!");
-            return ESP_ERR_NO_MEM;
-        }
-
-        ws_pkt.payload = buf;
-        ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
-        if (ret != ESP_OK) {
-            LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "httpd_ws_recv_frame failed with: " + std::to_string(ret) + "!");
-            free_psram_heap("websocket buf", buf);
-            return ret;
-        }
-        LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Got packet");
-        //LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Got packet with message: '" + std::string(ws_pkt.payload) + "'");
-    }
-
-    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "frame len is " + std::to_string(ws_pkt.len));
 
     return ESP_OK;
 }
@@ -150,15 +114,15 @@ static const httpd_uri_t ws_uri = {
 esp_err_t start_websocket_server(httpd_handle_t _server) {
     esp_err_t ret;
 
-    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Init Websocket Server");
+    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Init Websocket Server");
 
     server = _server;
 
     // Registering the ws handler
-    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Registering URI handler");
+    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Registering URI handler");
     ret = httpd_register_uri_handler(server, &ws_uri);
     if (ret != ESP_OK) {
-        LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Registering Websocket URI handler failed: " + std::to_string(ret));
+        LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Registering Websocket URI handler failed: " + std::to_string(ret));
     }
     return ret;
 }
