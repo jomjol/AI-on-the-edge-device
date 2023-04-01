59bf0d45d34c20758c46b451e0a06c69eb38a91c
CaCO3 <caco@ruinelli.ch>
2023-04-01 17:40:54 +0200

    STBI use PSRAM hooks

--- code/components/jomjol_image_proc/make_stb.cpp
+++ code/components/jomjol_image_proc/make_stb.cpp
@@ -1,7 +1,12 @@
 #include <stdint.h>
 #include <string>
+#include "psram.h"
 
 #include "../../include/defines.h"
+
+#define STBI_MALLOC(sz)           malloc_psram_heap("STBI", sz, MALLOC_CAP_SPIRAM)
+#define STBI_REALLOC(p,newsz)     realloc_psram_heap("STBI", p, newsz, MALLOC_CAP_SPIRAM)
+#define STBI_FREE(p)              free_psram_heap("STBI", p)
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../stb/stb_image.h"
