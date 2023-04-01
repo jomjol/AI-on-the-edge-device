547044ed4c02638c4674209a0d5bffba63b736d7
CaCO3 <caco@ruinelli.ch>
2023-04-01 17:31:50 +0200

    extend psram files

--- code/components/jomjol_helper/psram.cpp
+++ code/components/jomjol_helper/psram.cpp
@@ -11,10 +11,23 @@ void *malloc_psram_heap(std::string name, size_t size, uint32_t caps) {
 
 	ptr = heap_caps_malloc(size, caps);
     if (ptr != NULL) {
-	    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
+	    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
 	}
     else {
         LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to allocate " + to_string(size) + " bytes in PSRAM for '" + name + "'!");
+    }
+
+	return ptr;
+}
+
+
+void *realloc_psram_heap(std::string name, void *ptr, size_t size, uint32_t caps) {
+	ptr = heap_caps_realloc(ptr, size, caps);
+    if (ptr != NULL) {
+	    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Reallocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
+	}
+    else {
+        LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to reallocate " + to_string(size) + " bytes in PSRAM for '" + name + "'!");
     }
 
 	return ptr;
@@ -26,7 +39,7 @@ void *calloc_psram_heap(std::string name, size_t n, size_t size, uint32_t caps) 
 
 	ptr = heap_caps_calloc(n, size, caps);
     if (ptr != NULL) {
-	    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
+	    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Allocated " + to_string(size) + " bytes in PSRAM for '" + name + "'");
 	}
     else {
         LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "Failed to allocate " + to_string(size) + " bytes in PSRAM for '" + name + "'!");
@@ -37,6 +50,6 @@ void *calloc_psram_heap(std::string name, size_t n, size_t size, uint32_t caps) 
 
 
 void free_psram_heap(std::string name, void *ptr) {
-    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Freeing memory in PSRAM used for '" + name + "'...");
+    LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Freeing memory in PSRAM used for '" + name + "'...");
     heap_caps_free(ptr);
 }
--- code/components/jomjol_helper/psram.h
+++ code/components/jomjol_helper/psram.h
@@ -3,5 +3,6 @@
 
 void *malloc_psram_heap(std::string name, size_t size, uint32_t caps);
 void *calloc_psram_heap(std::string name, size_t n, size_t size, uint32_t caps);
+void *realloc_psram_heap(std::string name, void *ptr, size_t size, uint32_t caps);
 
 void free_psram_heap(std::string name, void *ptr);
