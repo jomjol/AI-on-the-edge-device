45c6feb9a7e2edadf45ef705bde6eb1415beb48b
CaCO3 <caco@ruinelli.ch>
2023-04-02 15:19:18 +0200

    enable PSRAM logging

--- code/components/jomjol_helper/psram.h
+++ code/components/jomjol_helper/psram.h
@@ -2,6 +2,7 @@
 #include "esp_heap_caps.h"
 
 void *malloc_psram_heap(std::string name, size_t size, uint32_t caps);
+void *realloc_psram_heap(std::string name, void *ptr, size_t size, uint32_t caps);
 void *calloc_psram_heap(std::string name, size_t n, size_t size, uint32_t caps);
 
 void free_psram_heap(std::string name, void *ptr);
