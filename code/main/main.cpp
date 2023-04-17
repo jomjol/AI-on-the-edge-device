a4ca0ea948b0959e3f3169613e64ddb737b15593
CaCO3 <caco@ruinelli.ch>
2023-04-17 23:01:26 +0200

    Fix return value check. It must be something else than ESP_FAIL, but it does not need to be ESP_OK!

--- code/main/main.cpp
+++ code/main/main.cpp
@@ -377,7 +377,7 @@ extern "C" void app_main(void)
     // Init external PSRAM
     // ********************************************
     esp_err_t PSRAMStatus = esp_psram_init();
-    if (PSRAMStatus != ESP_OK) {  // ESP_FAIL -> Failed to init PSRAM
+    if (PSRAMStatus == ESP_FAIL) {  // ESP_FAIL -> Failed to init PSRAM
         LogFile.WriteToFile(ESP_LOG_ERROR, TAG, "PSRAM init failed (" + std::to_string(PSRAMStatus) + ")! PSRAM not found or defective");
         setSystemStatusFlag(SYSTEM_STATUS_PSRAM_BAD);
         StatusLED(PSRAM_INIT, 1, true);
