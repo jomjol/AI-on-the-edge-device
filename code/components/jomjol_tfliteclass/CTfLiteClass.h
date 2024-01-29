9917243d316746125db754fb818338e127169e27
CaCO3 <caco@ruinelli.ch>
2024-01-29 23:10:09 +0100

    .

--- code/components/jomjol_tfliteclass/CTfLiteClass.h
+++ code/components/jomjol_tfliteclass/CTfLiteClass.h
@@ -8,7 +8,7 @@
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/kernels/micro_ops.h"
 
-#include "tensorflow/lite/micro/tflite_bridge/micro_error_reporter.h"
+//#include "tensorflow/lite/micro/tflite_bridge/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/kernels/micro_ops.h"
