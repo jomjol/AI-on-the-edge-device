ba7d1ea320120a9262d2e415d320535dd79c0657
CaCO3 <caco3@ruinelli.ch>
2024-02-03 00:32:40 +0100

    Update interface_mqtt.cpp

--- code/components/jomjol_mqtt/interface_mqtt.cpp
+++ code/components/jomjol_mqtt/interface_mqtt.cpp
@@ -198,7 +198,7 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event) {
 
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
-    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Event dispatched from event loop base=" + base + ", event_id=" + std::to_string(event_id));
+    LogFile.WriteToFile(ESP_LOG_DEBUG, TAG, "Event dispatched from event loop base=" + std::str(base) + ", event_id=" + std::to_string(event_id));
     mqtt_event_handler_cb((esp_mqtt_event_handle_t) event_data);
 }
 
