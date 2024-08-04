jomjol/AI-on-the-edge-device

commit 88b531ae8b89caaff6606a6ae68b8811d8eaac07

Camera settings (#3029)
* Add files via upload

* Add files via upload

* Add files via upload

* Add files via upload

* Add files via upload

* Add files via upload

* Add files via upload

* Add files via upload

code/components/jomjol_controlcamera/server_camera.cpp
@@ -5,6 +5,7 @@
 
 #include "esp_camera.h"
 #include "ClassControllCamera.h"
+#include "MainFlowControl.h"
 
 #include "ClassLogFile.h"
 #include "esp_log.h"
@@ -13,191 +14,183 @@
 
 static const char *TAG = "server_cam";
 
+void PowerResetCamera()
+{
+    ESP_LOGD(TAG, "Resetting camera by power down line");
+    gpio_config_t conf;
+    conf.intr_type = GPIO_INTR_DISABLE;
+    conf.pin_bit_mask = 1LL << GPIO_NUM_32;
+    conf.mode = GPIO_MODE_OUTPUT;
+    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&conf);
 
-void PowerResetCamera(){
-
-        ESP_LOGD(TAG, "Resetting camera by power down line");
-        gpio_config_t conf;
-        conf.intr_type = GPIO_INTR_DISABLE;
-        conf.pin_bit_mask = 1LL << GPIO_NUM_32;
-        conf.mode = GPIO_MODE_OUTPUT;
-        conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-        conf.pull_up_en = GPIO_PULLUP_DISABLE;
-        gpio_config(&conf);
-
-        // carefull, logic is inverted compared to reset pin
-        gpio_set_level(GPIO_NUM_32, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        gpio_set_level(GPIO_NUM_32, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // carefull, logic is inverted compared to reset pin
+    gpio_set_level(GPIO_NUM_32, 1);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    gpio_set_level(GPIO_NUM_32, 0);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
-
 
 esp_err_t handler_lightOn(httpd_req_t *req)
 {
-    #ifdef DEBUG_DETAIL_ON   
-        LogFile.WriteHeapInfo("handler_lightOn - Start");
-        ESP_LOGD(TAG, "handler_lightOn uri: %s", req->uri);
-    #endif
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_lightOn - Start");
+    ESP_LOGD(TAG, "handler_lightOn uri: %s", req->uri);
+#endif
 
-    if (Camera.getCameraInitSuccessful()) 
+    if (Camera.getCameraInitSuccessful())
     {
         Camera.LightOnOff(true);
-        const char* resp_str = (const char*) req->user_ctx;
+        const char *resp_str = (const char *)req->user_ctx;
         httpd_resp_send(req, resp_str, strlen(resp_str));
     }
-    else 
+    else
     {
         httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "Camera not initialized: REST API /lighton not available!");
         return ESP_ERR_NOT_FOUND;
     }
 
-    #ifdef DEBUG_DETAIL_ON   
-        LogFile.WriteHeapInfo("handler_lightOn - Done");
-    #endif
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_lightOn - Done");
+#endif
 
     return ESP_OK;
 }
 
-
 esp_err_t handler_lightOff(httpd_req_t *req)
 {
-    #ifdef DEBUG_DETAIL_ON   
-        LogFile.WriteHeapInfo("handler_lightOff - Start");
-        ESP_LOGD(TAG, "handler_lightOff uri: %s", req->uri);
-    #endif
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_lightOff - Start");
+    ESP_LOGD(TAG, "handler_lightOff uri: %s", req->uri);
+#endif
 
-    if (Camera.getCameraInitSuccessful()) 
+    if (Camera.getCameraInitSuccessful())
     {
         Camera.LightOnOff(false);
-        const char* resp_str = (const char*) req->user_ctx;
-        httpd_resp_send(req, resp_str, strlen(resp_str));       
+        const char *resp_str = (const char *)req->user_ctx;
+        httpd_resp_send(req, resp_str, strlen(resp_str));
     }
-    else 
+    else
     {
         httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "Camera not initialized: REST API /lightoff not available!");
         return ESP_ERR_NOT_FOUND;
     }
 
-    #ifdef DEBUG_DETAIL_ON   
-        LogFile.WriteHeapInfo("handler_lightOff - Done");
-    #endif
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_lightOff - Done");
+#endif
 
     return ESP_OK;
 }
 
-
 esp_err_t handler_capture(httpd_req_t *req)
 {
-    #ifdef DEBUG_DETAIL_ON   
-        LogFile.WriteHeapInfo("handler_capture - Start");
-    #endif
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_capture - Start");
+#endif
 
-    if (Camera.getCameraInitSuccessful()) 
+    if (Camera.getCameraInitSuccessful())
     {
-        int quality;
-        framesize_t res;
-        bool zoomEnabled;
-        int zoomMode;
-        int zoomOffsetX;
-        int zoomOffsetY;
+#ifdef DEBUG_DETAIL_ON
+        ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
+#endif
 
-        Camera.GetCameraParameter(req, quality, res, zoomEnabled, zoomMode, zoomOffsetX, zoomOffsetY);
-
-        #ifdef DEBUG_DETAIL_ON
-            ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
-        #endif
-
-        Camera.SetQualitySize(quality, res, zoomEnabled, zoomMode, zoomOffsetX, zoomOffsetY);
+        // wenn die Kameraeinstellungen durch Erstellen eines neuen Referenzbildes verändert wurden, müssen sie neu gesetzt werden
+        if (CFstatus.changedCameraSettings)
+        {
+            Camera.setSensorDatenFromCCstatus(); // CCstatus >>> Kamera
+            Camera.SetQualityZoomSize(CCstatus.ImageQuality, CCstatus.ImageFrameSize, CCstatus.ImageZoomEnabled, CCstatus.ImageZoomOffsetX, CCstatus.ImageZoomOffsetY, CCstatus.ImageZoomSize);
+            CFstatus.changedCameraSettings = false;
+        }
 
         esp_err_t result;
         result = Camera.CaptureToHTTP(req);
 
-        #ifdef DEBUG_DETAIL_ON   
-            LogFile.WriteHeapInfo("handler_capture - Done");
-        #endif
+#ifdef DEBUG_DETAIL_ON
+        LogFile.WriteHeapInfo("handler_capture - Done");
+#endif
 
         return result;
     }
-        else 
+    else
     {
         httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "Camera not initialized: REST API /capture not available!");
         return ESP_ERR_NOT_FOUND;
     }
 }
 
-
 esp_err_t handler_capture_with_light(httpd_req_t *req)
 {
-    #ifdef DEBUG_DETAIL_ON  
-        LogFile.WriteHeapInfo("handler_capture_with_light - Start");
-    #endif
-    
-    if (Camera.getCameraInitSuccessful()) 
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_capture_with_light - Start");
+#endif
+
+    if (Camera.getCameraInitSuccessful())
     {
         char _query[100];
         char _delay[10];
-
-        int quality;
-        framesize_t res;    
-        bool zoomEnabled;
-        int zoomMode;
-        int zoomOffsetX;
-        int zoomOffsetY;
         int delay = 2500;
 
         if (httpd_req_get_url_query_str(req, _query, 100) == ESP_OK)
         {
             ESP_LOGD(TAG, "Query: %s", _query);
+
             if (httpd_query_key_value(_query, "delay", _delay, 10) == ESP_OK)
             {
-                #ifdef DEBUG_DETAIL_ON   
-                    ESP_LOGD(TAG, "Delay: %s", _delay);
-                #endif        
+#ifdef DEBUG_DETAIL_ON
+                ESP_LOGD(TAG, "Delay: %s", _delay);
+#endif
                 delay = atoi(_delay);
 
                 if (delay < 0)
+                {
                     delay = 0;
+                }
             }
         }
 
-        Camera.GetCameraParameter(req, quality, res, zoomEnabled, zoomMode, zoomOffsetX, zoomOffsetY);
+#ifdef DEBUG_DETAIL_ON
+        ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
+#endif
 
-        #ifdef DEBUG_DETAIL_ON
-            ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
-        #endif
+        // wenn die Kameraeinstellungen durch Erstellen eines neuen Referenzbildes verändert wurden, müssen sie neu gesetzt werden
+        if (CFstatus.changedCameraSettings)
+        {
+            Camera.setSensorDatenFromCCstatus(); // CCstatus >>> Kamera
+            Camera.SetQualityZoomSize(CCstatus.ImageQuality, CCstatus.ImageFrameSize, CCstatus.ImageZoomEnabled, CCstatus.ImageZoomOffsetX, CCstatus.ImageZoomOffsetY, CCstatus.ImageZoomSize);
+            CFstatus.changedCameraSettings = false;
+        }
 
-        Camera.SetQualitySize(quality, res, zoomEnabled, zoomMode, zoomOffsetX, zoomOffsetY);
         Camera.LightOnOff(true);
         const TickType_t xDelay = delay / portTICK_PERIOD_MS;
-        vTaskDelay( xDelay );
+        vTaskDelay(xDelay);
 
         esp_err_t result;
-        result = Camera.CaptureToHTTP(req);  
+        result = Camera.CaptureToHTTP(req);
 
         Camera.LightOnOff(false);
 
-        #ifdef DEBUG_DETAIL_ON   
-            LogFile.WriteHeapInfo("handler_capture_with_light - Done");
-        #endif
+#ifdef DEBUG_DETAIL_ON
+        LogFile.WriteHeapInfo("handler_capture_with_light - Done");
+#endif
 
         return result;
     }
-        else 
+    else
     {
         httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "Camera not initialized: REST API /capture_with_flashlight not available!");
         return ESP_ERR_NOT_FOUND;
     }
 }
 
-
 esp_err_t handler_capture_save_to_file(httpd_req_t *req)
 {
-    #ifdef DEBUG_DETAIL_ON   
-        LogFile.WriteHeapInfo("handler_capture_save_to_file - Start");
-    #endif
+#ifdef DEBUG_DETAIL_ON
+    LogFile.WriteHeapInfo("handler_capture_save_to_file - Start");
+#endif
 
-    if (Camera.getCameraInitSuccessful()) 
+    if (Camera.getCameraInitSuccessful())
     {
         char _query[100];
         char _delay[10];
@@ -205,98 +198,102 @@ esp_err_t handler_capture_save_to_file(httpd_req_t *req)
         char filename[100];
         std::string fn = "/sdcard/";
 
-
-        int quality;
-        framesize_t res;    
-        bool zoomEnabled;
-        int zoomMode;
-        int zoomOffsetX;
-        int zoomOffsetY;
-
         if (httpd_req_get_url_query_str(req, _query, 100) == ESP_OK)
         {
             ESP_LOGD(TAG, "Query: %s", _query);
+
             if (httpd_query_key_value(_query, "filename", filename, 100) == ESP_OK)
             {
                 fn.append(filename);
-                #ifdef DEBUG_DETAIL_ON   
-                    ESP_LOGD(TAG, "Filename: %s", fn.c_str());
-                #endif
+#ifdef DEBUG_DETAIL_ON
+                ESP_LOGD(TAG, "Filename: %s", fn.c_str());
+#endif
             }
             else
+            {
                 fn.append("noname.jpg");
+            }
 
             if (httpd_query_key_value(_query, "delay", _delay, 10) == ESP_OK)
             {
-                #ifdef DEBUG_DETAIL_ON   
-                    ESP_LOGD(TAG, "Delay: %s", _delay);
-                #endif
+#ifdef DEBUG_DETAIL_ON
+                ESP_LOGD(TAG, "Delay: %s", _delay);
+#endif
                 delay = atoi(_delay);
 
                 if (delay < 0)
+                {
                     delay = 0;
+                }
             }
         }
         else
+        {
             fn.append("noname.jpg");
+        }
 
-        Camera.GetCameraParameter(req, quality, res, zoomEnabled, zoomMode, zoomOffsetX, zoomOffsetY);
-        #ifdef DEBUG_DETAIL_ON   
-            ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
-        #endif
-        Camera.SetQualitySize(quality, res, zoomEnabled, zoomMode, zoomOffsetX, zoomOffsetY);
+#ifdef DEBUG_DETAIL_ON
+        ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
+#endif
+
+        // wenn die Kameraeinstellungen durch Erstellen eines neuen Referenzbildes verändert wurden, müssen sie neu gesetzt werden
+        if (CFstatus.changedCameraSettings)
+        {
+            Camera.setSensorDatenFromCCstatus(); // CCstatus >>> Kamera
+            Camera.SetQualityZoomSize(CCstatus.ImageQuality, CCstatus.ImageFrameSize, CCstatus.ImageZoomEnabled, CCstatus.ImageZoomOffsetX, CCstatus.ImageZoomOffsetY, CCstatus.ImageZoomSize);
+            CFstatus.changedCameraSettings = false;
+        }
 
         esp_err_t result;
-        result = Camera.CaptureToFile(fn, delay);  
+        result = Camera.CaptureToFile(fn, delay);
 
-        const char* resp_str = (const char*) fn.c_str();
+        const char *resp_str = (const char *)fn.c_str();
         httpd_resp_send(req, resp_str, strlen(resp_str));
 
-        #ifdef DEBUG_DETAIL_ON   
-            LogFile.WriteHeapInfo("handler_capture_save_to_file - Done");
-        #endif
+#ifdef DEBUG_DETAIL_ON
+        LogFile.WriteHeapInfo("handler_capture_save_to_file - Done");
+#endif
 
         return result;
     }
-    else 
+    else
     {
         httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "Camera not initialized: REST API /save not available!");
         return ESP_ERR_NOT_FOUND;
     }
 }
 
-
 void register_server_camera_uri(httpd_handle_t server)
 {
-#ifdef DEBUG_DETAIL_ON   
+#ifdef DEBUG_DETAIL_ON
     ESP_LOGI(TAG, "server_part_camera - Registering URI handlers");
 #endif
 
-    httpd_uri_t camuri = { };
-    camuri.method    = HTTP_GET;
+    httpd_uri_t camuri = {};
+    camuri.method = HTTP_GET;
 
-    camuri.uri       = "/lighton";
-    camuri.handler   = handler_lightOn;
-    camuri.user_ctx  = (void*) "Light On";    
+    camuri.uri = "/lighton";
+    camuri.handler = handler_lightOn;
+    camuri.user_ctx = (void *)"Light On";
     httpd_register_uri_handler(server, &camuri);
 
-    camuri.uri       = "/lightoff";
-    camuri.handler   = handler_lightOff;
-    camuri.user_ctx  = (void*) "Light Off"; 
-    httpd_register_uri_handler(server, &camuri);    
+    camuri.uri = "/lightoff";
+    camuri.handler = handler_lightOff;
+    camuri.user_ctx = (void *)"Light Off";
+    httpd_register_uri_handler(server, &camuri);
 
-    camuri.uri       = "/capture";
-    camuri.handler   = handler_capture;
-    camuri.user_ctx  = NULL; 
-    httpd_register_uri_handler(server, &camuri);      
+    camuri.uri = "/capture";
+    camuri.handler = handler_capture;
+    camuri.user_ctx = NULL;
+    httpd_register_uri_handler(server, &camuri);
 
-    camuri.uri       = "/capture_with_flashlight";
-    camuri.handler   = handler_capture_with_light;
-    camuri.user_ctx  = NULL; 
-    httpd_register_uri_handler(server, &camuri);  
+    camuri.uri = "/capture_with_flashlight";
+    camuri.handler = handler_capture_with_light;
+    camuri.user_ctx = NULL;
+    httpd_register_uri_handler(server, &camuri);
 
-    camuri.uri       = "/save";
-    camuri.handler   = handler_capture_save_to_file;
-    camuri.user_ctx  = NULL; 
-    httpd_register_uri_handler(server, &camuri);    
+    camuri.uri = "/save";
+    camuri.handler = handler_capture_save_to_file;
+    camuri.user_ctx = NULL;
+    httpd_register_uri_handler(server, &camuri);
 }

code/components/jomjol_controlcamera/ClassControllCamera.h
@@ -15,66 +15,100 @@
 #include "CImageBasis.h"
 #include "../../include/defines.h"
 
-class CCamera {
-    protected:
-        int ActualQuality;
-        framesize_t ActualResolution;
-        int brightness, contrast, saturation, autoExposureLevel;
-        bool isFixedExposure;
-        int waitbeforepicture_org;
-        int led_intensity = 4095;
+typedef enum
+{
+    OV2640_MODE_UXGA,
+    OV2640_MODE_SVGA,
+    OV2640_MODE_CIF
+} ov2640_sensor_mode_t;
 
-        void ledc_init(void);
-        bool CameraInitSuccessful = false;
-        bool demoMode = false;
+typedef struct
+{
+    framesize_t ImageFrameSize = FRAMESIZE_VGA; // 0 - 10
+    gainceiling_t ImageGainceiling;             // Image gain (GAINCEILING_x2, x4, x8, x16, x32, x64 or x128)
 
-        bool loadNextDemoImage(camera_fb_t *fb);
-        long GetFileSize(std::string filename);
+    int ImageQuality;    // 0 - 63
+    int ImageBrightness; // (-2 to 2) - set brightness
+    int ImageContrast;   //-2 - 2
+    int ImageSaturation; //-2 - 2
+    int ImageSharpness;  //-2 - 2
+    bool ImageAutoSharpness;
+    int ImageSpecialEffect; // 0 - 6
+    int ImageWbMode;        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+    int ImageAwb;           // white balance enable (0 or 1)
+    int ImageAwbGain;       // Auto White Balance enable (0 or 1)
+    int ImageAec;           // auto exposure off (1 or 0)
+    int ImageAec2;          // automatic exposure sensor  (0 or 1)
+    int ImageAeLevel;       // auto exposure levels (-2 to 2)
+    int ImageAecValue;      // set exposure manually  (0-1200)
+    int ImageAgc;           // auto gain off (1 or 0)
+    int ImageAgcGain;       // set gain manually (0 - 30)
+    int ImageBpc;           // black pixel correction
+    int ImageWpc;           // white pixel correction
+    int ImageRawGma;        // (1 or 0)
+    int ImageLenc;          // lens correction (1 or 0)
+    int ImageHmirror;       // (0 or 1) flip horizontally
+    int ImageVflip;         // Invert image (0 or 1)
+    int ImageDcw;           // downsize enable (1 or 0)
 
-        void SetCamWindow(sensor_t *s, int resolution, int xOffset, int yOffset, int xLength, int yLength);
-        void SetImageWidthHeightFromResolution(framesize_t resol);
+    int ImageWidth;
+    int ImageHeight;
 
-    public:
-        int image_height, image_width;
-        bool imageZoomEnabled = false;
-        int imageZoomMode = 0;
-        int imageZoomOffsetX = 0;
-        int imageZoomOffsetY = 0;
-        bool imageNegative = false;
-        bool imageAec2 = false;
-        bool imageAutoSharpness = false;
-        int imageSharpnessLevel = 0;
-    #ifdef GRAYSCALE_AS_DEFAULT
-        bool imageGrayscale = true;
-    #else
-        bool imageGrayscale = false;
-    #endif
-        
-        CCamera();
-        esp_err_t InitCam();
+    int ImageLedIntensity;
 
-        void LightOnOff(bool status);
-        void LEDOnOff(bool status);
-        esp_err_t CaptureToHTTP(httpd_req_t *req, int delay = 0);
-        esp_err_t CaptureToStream(httpd_req_t *req, bool FlashlightOn);
-        void SetQualitySize(int qual, framesize_t resol, bool zoomEnabled, int zoomMode, int zoomOffsetX, int zoomOffsetY);
-        bool SetBrightnessContrastSaturation(int _brightness, int _contrast, int _saturation, int _autoExposureLevel, bool _grayscale, bool _negative, bool _aec2, int _sharpnessLevel);
-        void SetZoom(bool zoomEnabled, int zoomMode, int zoomOffsetX, int zoomOffsetY);
-        void GetCameraParameter(httpd_req_t *req, int &qual, framesize_t &resol, bool &zoomEnabled, int &zoomMode, int &zoomOffsetX, int &zoomOffsetY);
-        void SetLEDIntensity(float _intrel);
-        bool testCamera(void);
-        void EnableAutoExposure(int flash_duration);
-        bool getCameraInitSuccessful();
-        void useDemoMode(void);
-       
+    bool ImageZoomEnabled;
+    int ImageZoomMode;
+    int ImageZoomOffsetX;
+    int ImageZoomOffsetY;
+    int ImageZoomSize;
 
-        framesize_t TextToFramesize(const char * text);
+    int WaitBeforePicture;
+    bool isImageSize;
 
-        esp_err_t CaptureToFile(std::string nm, int delay = 0);
-        esp_err_t CaptureToBasisImage(CImageBasis *_Image, int delay = 0);
+    bool CameraInitSuccessful;
+    bool changedCameraSettings;
+    bool DemoMode;
+    bool SaveAllFiles;
+} camera_controll_config_temp_t;
+
+extern camera_controll_config_temp_t CCstatus;
+
+class CCamera
+{
+protected:
+    void ledc_init(void);
+    bool loadNextDemoImage(camera_fb_t *fb);
+    long GetFileSize(std::string filename);
+    void SetCamWindow(sensor_t *s, int resolution, int xOffset, int yOffset, int xTotal, int yTotal, int xOutput, int yOutput);
+    void SetImageWidthHeightFromResolution(framesize_t resol);
+
+public:
+    CCamera(void);
+    esp_err_t InitCam(void);
+	
+    void LightOnOff(bool status);
+    void LEDOnOff(bool status);
+
+    esp_err_t setSensorDatenFromCCstatus(void);
+    esp_err_t getSensorDatenToCCstatus(void);
+
+    esp_err_t CaptureToHTTP(httpd_req_t *req, int delay = 0);
+    esp_err_t CaptureToStream(httpd_req_t *req, bool FlashlightOn);
+
+    void SetQualityZoomSize(int qual, framesize_t resol, bool zoomEnabled, int zoomOffsetX, int zoomOffsetY, int imageSize);
+    void SetZoomSize(bool zoomEnabled, int zoomOffsetX, int zoomOffsetY, int imageSize);
+    void SetCamSharpness(bool _autoSharpnessEnabled, int _sharpnessLevel);
+    
+	void SetLEDIntensity(float _intrel);
+    bool testCamera(void);
+    bool getCameraInitSuccessful(void);
+    void useDemoMode(void);
+
+    framesize_t TextToFramesize(const char *text);
+	
+    esp_err_t CaptureToFile(std::string nm, int delay = 0);
+    esp_err_t CaptureToBasisImage(CImageBasis *_Image, int delay = 0);
 };
 
-
 extern CCamera Camera;
-
 #endif

code/components/jomjol_controlcamera/ov2640_sharpness.cpp
@@ -3,71 +3,78 @@
 #include "ov2640_sharpness.h"
 
 
-#define OV2640_MAXLEVEL_SHARPNESS 6
-
 const static uint8_t OV2640_SHARPNESS_AUTO[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
     0x93, 0x20, 0x20,
     0x00, 0x00, 0x00
 };
 
 const static uint8_t OV2640_SHARPNESS_MANUAL[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
     0x93, 0x00, 0x20,
     0x00, 0x00, 0x00
 };
 
 const static uint8_t OV2640_SHARPNESS_LEVEL0[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xc0, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xC0, 0x1F,
     0x00, 0x00, 0x00
 };
 const static uint8_t OV2640_SHARPNESS_LEVEL1[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xc1, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xC1, 0x1F,
     0x00, 0x00, 0x00
 };
 const static uint8_t OV2640_SHARPNESS_LEVEL2[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xc2, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xC2, 0x1F,
     0x00, 0x00, 0x00
 };
 const static uint8_t OV2640_SHARPNESS_LEVEL3[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xc4, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xC4, 0x1F,
     0x00, 0x00, 0x00
 };
 const static uint8_t OV2640_SHARPNESS_LEVEL4[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xc8, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xC8, 0x1F,
     0x00, 0x00, 0x00
 };
 const static uint8_t OV2640_SHARPNESS_LEVEL5[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xd0, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xD0, 0x1F,
     0x00, 0x00, 0x00
 };
 const static uint8_t OV2640_SHARPNESS_LEVEL6[]=
 {
-    0xFF, 0x00, 0xff,
-    0x92, 0x01, 0xff,
-    0x93, 0xdf, 0x1f,
+	//reg, val, mask
+    0xFF, 0x00, 0xFF,
+    0x92, 0x01, 0xFF,
+    0x93, 0xDF, 0x1F,
     0x00, 0x00, 0x00
 };
 
@@ -82,6 +89,9 @@ const static uint8_t *OV2640_SETTING_SHARPNESS[]=
     OV2640_SHARPNESS_LEVEL6  // +3 sharpness
 };
 
+#define OV2640_MAXLEVEL_SHARPNESS 6
+
+
 static int table_mask_write(sensor_t *sensor, const uint8_t* ptab)
 {
     uint8_t address;
@@ -91,34 +101,52 @@ static int table_mask_write(sensor_t *sensor, const uint8_t* ptab)
     const uint8_t *pdata = ptab;
 
     if (pdata == NULL)
+	{
         return -1;
+	}
 
     while (1)
     {   
         address = *pdata++;
         value = *pdata++;
         mask = *pdata++;
+		
         if ((address == 0) && (value == 0) && (mask == 0))
+		{
             break;
+		}
+		
         sensor->set_reg(sensor, address, mask, value);
     }   
 
     return 0;
 }
 
+
 int ov2640_enable_auto_sharpness(sensor_t *sensor)
 {
     table_mask_write(sensor, OV2640_SHARPNESS_AUTO);
+	
     return 0;
 }
 
 
 int ov2640_set_sharpness(sensor_t *sensor, int sharpness)
 {
-    if ((sharpness < -3) || (sharpness > OV2640_MAXLEVEL_SHARPNESS - 3))
-        return -1;
+	int sharpness_temp = 0;
+	
+    if (sharpness < -3)
+	{
+        sharpness_temp = -3;
+	}
+	
+    if (sharpness > OV2640_MAXLEVEL_SHARPNESS - 3)
+	{
+        sharpness_temp = OV2640_MAXLEVEL_SHARPNESS - 3;
+	}	
 
     table_mask_write(sensor, OV2640_SHARPNESS_MANUAL);
-    table_mask_write(sensor, OV2640_SETTING_SHARPNESS[sharpness + 3]);
+    table_mask_write(sensor, OV2640_SETTING_SHARPNESS[sharpness_temp + 3]);
+	
     return 0;
 }

code/components/jomjol_controlcamera/server_camera.h
@@ -10,7 +10,6 @@
 //#include "ClassControllCamera.h"
 
 void register_server_camera_uri(httpd_handle_t server);
-
 void PowerResetCamera();
 
 #endif

commit b0795ad2c881476b9d6fdb30200f760db29cbc71

Fix building with `-D DEBUG_DETAIL_ON`
Building with `-D DEBUG_DETAIL_ON` has been broken since #3029.

code/components/jomjol_controlcamera/server_camera.cpp
@@ -92,10 +92,6 @@ esp_err_t handler_capture(httpd_req_t *req)
 
     if (Camera.getCameraInitSuccessful())
     {
-#ifdef DEBUG_DETAIL_ON
-        ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
-#endif
-
         // wenn die Kameraeinstellungen durch Erstellen eines neuen Referenzbildes verändert wurden, müssen sie neu gesetzt werden
         if (CFstatus.changedCameraSettings)
         {
@@ -149,10 +145,6 @@ esp_err_t handler_capture_with_light(httpd_req_t *req)
                 }
             }
         }
-
-#ifdef DEBUG_DETAIL_ON
-        ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
-#endif
 
         // wenn die Kameraeinstellungen durch Erstellen eines neuen Referenzbildes verändert wurden, müssen sie neu gesetzt werden
         if (CFstatus.changedCameraSettings)
@@ -231,10 +223,6 @@ esp_err_t handler_capture_save_to_file(httpd_req_t *req)
         {
             fn.append("noname.jpg");
         }
-
-#ifdef DEBUG_DETAIL_ON
-        ESP_LOGD(TAG, "Size: %d, Quality: %d", res, quality);
-#endif
 
         // wenn die Kameraeinstellungen durch Erstellen eines neuen Referenzbildes verändert wurden, müssen sie neu gesetzt werden
         if (CFstatus.changedCameraSettings)